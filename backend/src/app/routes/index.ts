87001754380d5b8014a6b63b53f2e4fa2e13fdaf
abrakib942 <abrakib942@gmail.com>
2024-10-01 23:19:16 +0600

    auth and user module

---
+++ backend/src/app/routes/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.routes';
+import { UserRoutes } from '../modules/user/user.routes';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/auth',
+    routes: AuthRoutes,
+  },
+  {
+    path: '/user',
+    routes: UserRoutes,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
+export default router;
